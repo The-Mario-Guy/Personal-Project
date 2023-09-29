530aa9136143bded5fe5e4e152ab82cfbe9e97e4
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-09-29 12:37:43 -0700

    Created Stamina Bar
    Needs work

--- Personal-Project/Assets/Scripts/PlayerController.cs
+++ Personal-Project/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     CharacterController characterController;
@@ -9,16 +10,20 @@ public class PlayerController : MonoBehaviour
     public float RunningSpeed = 5;
     public float Gravity = 9.8f;
     private float velocity = 0;
+    private float _totalStamina = 100;
+    public bool _isRunning = false;
 
     private Camera cam;
     public bool isFading;
     private Animator _Fade;
+    [SerializeField] private Slider _stamina;
  
     private void Start()
     {
         cam = Camera.main;
         characterController = GetComponent<CharacterController>();
         _Fade = GetComponent<Animator>();
+        _stamina = GameObject.Find("Stamina Bar").GetComponent<Slider>();
     }
  
     void Update()
@@ -27,7 +32,7 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
         float vertical = Input.GetAxis("Vertical") * MovementSpeed;
         characterController.Move((cam.transform.right * horizontal + cam.transform.forward * vertical) * Time.deltaTime);
- 
+        _stamina.value = _totalStamina;
         // Gravity
         if(characterController.isGrounded)
         {
@@ -44,19 +49,55 @@ public class PlayerController : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+       
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            MovementSpeed = RunningSpeed;
+            isRunning();
+            updateStaminaBar(-2);
         }
-        else
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            MovementSpeed = 3;
+            stopRunning();
+            
+        }
+        StaminaRegenRoutine();
+    }
+
+    private void isRunning()
+    {
+       _isRunning = true;
+        MovementSpeed = RunningSpeed;
+    }
+    private void stopRunning()
+    {
+        _isRunning = false;
+        MovementSpeed = 3;
+       
+    }
+    public void updateStaminaBar(float currentStamina)
+    {
+        if (_totalStamina - currentStamina <= 0)
+        {
+            stopRunning();
+        }
+        _totalStamina += currentStamina;
+    }
+    //Decreases Stamina Bar
+    IEnumerator StaminaRoutine()
+    {
+        while(_isRunning == true)
+        {
+            updateStaminaBar(-20);
+            yield return new WaitForSeconds(1.0f);
         }
     }
-   /* private void OnTriggerEnter(Collider collision) 
+    //Increases Stamina Bar
+    IEnumerator StaminaRegenRoutine()
     {
-       
-        
-    }  */
-
+        while (_isRunning == false) 
+        {
+            updateStaminaBar(+20);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
 }
