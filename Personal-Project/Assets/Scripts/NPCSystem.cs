748210b670f8cc730ce3b5cf258097fd0175c315
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-10-04 12:06:10 -0700

    Update NPCSystem.cs

--- Personal-Project/Assets/Scripts/NPCSystem.cs
+++ Personal-Project/Assets/Scripts/NPCSystem.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class NPCSystem : MonoBehaviour
 {
     bool player_detection = false;  
@@ -9,10 +9,15 @@ public class NPCSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player_detection && Input.GetMouseButtonDown(0))
+        if(player_detection && Input.GetMouseButtonDown(0) && !PlayerController.dialouge)
         {
-            print("The Talker");
+            PlayerController.dialouge = true;
         }
+    }
+    void NewDialouge(string text)
+    {
+        //Continue from 1:32 on video vv
+        /* https://www.youtube.com/watch?v=a9bhK-P7r-0&ab_channel=KaiJPR */
     }
 
     private void OnTriggerEnter(Collider other)
